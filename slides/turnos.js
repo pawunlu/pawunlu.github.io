2e17501f3c575e9fa2bc80393e5ec936b4c33216
sricci.soft <tomasdelvechio-actions-syn-repos@example.org>
2024-04-04 22:21:54 +0000

    auto sync of repo with tomasdelvechio/actions-push-repo-to-subdir

---
+++ slides/turnos.js
@@ -0,0 +1,283 @@
+{
+    "especialistas": [
+        {
+            "matricula": "3737",
+            "nombre": "Tekito",
+            "apellido": "Lakarie",
+            "diasQueAtiende": [
+                "Lunes",
+                "Martes",
+                "Jueves"
+            ],
+            "horarioInicio": {
+                "horas": 9,
+                "minutos": 0
+            },
+            "horarioFinalizacion": {
+                "horas": 12,
+                "minutos": 0
+            },
+            "duracionTurno": 20,
+            "turnosTomados": [
+                {
+                    "dia": "Lunes",
+                    "horas": 9,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 9,
+                    "minutos": 20
+                },
+                {
+                    "dia": "Martes",
+                    "horas": 10,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Martes",
+                    "horas": 10,
+                    "minutos": 40
+                },
+                {
+                    "dia": "Martes",
+                    "horas": 11,
+                    "minutos": 20
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 9,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 9,
+                    "minutos": 20
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 10,
+                    "minutos": 40
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 11,
+                    "minutos": 40
+                }
+            ]
+        },
+        {
+            "matricula": "2222",
+            "nombre": "Tarayado",
+            "apellido": "Tukoko",
+            "diasQueAtiende": [
+                "Martes",
+                "Jueves",
+                "Viernes"
+            ],
+            "horarioInicio": {
+                "horas": 13,
+                "minutos": 0
+            },
+            "horarioFinalizacion": {
+                "horas": 18,
+                "minutos": 0
+            },
+            "duracionTurno": 30,
+            "turnosTomados": [
+                {
+                    "dia": "Martes",
+                    "horas": 14,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Martes",
+                    "horas": 14,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Martes",
+                    "horas": 16,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 16,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 17,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 17,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 13,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 16,
+                    "minutos": 30
+                }
+            ]
+        },
+        {
+            "matricula": "20202",
+            "nombre": "Tekuro",
+            "apellido": "Lakria",
+            "diasQueAtiende": [
+                "Lunes",
+                "Jueves",
+                "Viernes"
+            ],
+            "horarioInicio": {
+                "horas": 10,
+                "minutos": 0
+            },
+            "horarioFinalizacion": {
+                "horas": 14,
+                "minutos": 0
+            },
+            "duracionTurno": 25,
+            "turnosTomados": [
+                {
+                    "dia": "Lunes",
+                    "horas": 11,
+                    "minutos": 15
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 11,
+                    "minutos": 40
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 12,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Jueves",
+                    "horas": 10,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 12,
+                    "minutos": 55
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 13,
+                    "minutos": 20
+                }
+            ]
+        },
+        {
+            "matricula": "9595",
+            "nombre": "Isee",
+            "apellido": "Deadpeople",
+            "diasQueAtiende": [
+                "Lunes",
+                "Miercoles",
+                "Viernes"
+            ],
+            "horarioInicio": {
+                "horas": 15,
+                "minutos": 0
+            },
+            "horarioFinalizacion": {
+                "horas": 18,
+                "minutos": 0
+            },
+            "duracionTurno": 10,
+            "turnosTomados": [
+                {
+                    "dia": "Lunes",
+                    "horas": 15,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 15,
+                    "minutos": 10
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 15,
+                    "minutos": 20
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 15,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Lunes",
+                    "horas": 15,
+                    "minutos": 50
+                },
+                {
+                    "dia": "Miercoles",
+                    "horas": 14,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Miercoles",
+                    "horas": 14,
+                    "minutos": 50
+                },
+                {
+                    "dia": "Miercoles",
+                    "horas": 16,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Miercoles",
+                    "horas": 17,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 14,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 15,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 16,
+                    "minutos": 30
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 16,
+                    "minutos": 40
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 16,
+                    "minutos": 50
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 17,
+                    "minutos": 0
+                },
+                {
+                    "dia": "Viernes",
+                    "horas": 17,
+                    "minutos": 30
+                }
+            ]
+        }
+    ]
+}
